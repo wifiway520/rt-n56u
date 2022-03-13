c2b02f33c999cf3d6388d6e205ea76a1b4d3a79c
wifiway520 <54652920+wifiway520@users.noreply.github.com>
2022-03-14 01:10:03 +0800

    Add files via upload

---
+++ trunk/configs/boards/MT1300/board.h
@@ -0,0 +1,35 @@
+/* GL-MT1300 */
+
+#define BOARD_PID		"MT1300"
+#define BOARD_NAME		"MT1300"
+#define BOARD_DESC		"GL-MT1300 Wireless Router"
+#define BOARD_VENDOR_NAME	"GL.INET"
+#define BOARD_VENDOR_URL	"https://www.gl-inet.com/"
+#define BOARD_MODEL_URL		"https://www.gl-inet.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#undef BOARD_GPIO_BTN_FN1
+#define BOARD_GPIO_BTN_RESET 18
+#define BOARD_GPIO_BTN_WPS 16
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#undef   BOARD_GPIO_LED_SW2G
+#undef   BOARD_GPIO_LED_SW5G
+#undef  BOARD_GPIO_LED_POWER
+#undef  BOARD_GPIO_LED_LAN
+#define BOARD_GPIO_LED_WAN	14
+#undef BOARD_GPIO_LED_USB
+#undef BOARD_GPIO_LED_ROUTER
+#undef BOARD_GPIO_PWR_USB_ON
+#undef BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY    3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3           1
+#define BOARD_USB_PORT_SWAP	0
